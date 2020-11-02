a68dfc35c6cbbc7c324b85ef2d60742fbf2001c1
pjaeyoung <saiei2484@gmail.com>
2020-11-02 22:35:35 +0900

    feat : register api 작성

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const { User } = require('./models/User');
+
 const app = express();
 const port = 3000;
 
@@ -17,6 +19,18 @@ mongoose
 	.then(() => console.log('MongoDB connected'))
 	.catch((err) => console.log(err));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/register', (req, res) => {
+	// 회원가입에 필요한 정보들을 client에서 가져와 데이터베이스에 저장
+	const user = new User(req.body);
+	user.save((err, userInfo) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({ success: true });
+	});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
